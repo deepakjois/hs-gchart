3971e5d89908fee2dee23bc30553cb444baf548c
Deepak Jois <deepak.jois@gmail.com>
2010-01-31 18:37:57 -0800

    Using my own Monad

--- Types.hs
+++ Types.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeSynonymInstances #-}
 import Control.Monad.State
 import Data.List
 import Data.Char (chr, ord)
@@ -9,19 +10,27 @@ data ChartType = Line |
                  Sparklines |
                  LineXY deriving Show
 
+type ChartTitle = Maybe String
+
+                            
 data ChartData = D [Int] |
                  XY [(Int,Int)] deriving Show
 
 data Chart = Chart { chartSize  :: ChartSize,
                      chartType  :: ChartType,
-                     chartData  :: [ChartData],
-                     chartTitle :: Maybe String } deriving Show
+                     chartTitle :: ChartTitle,
+                     chartData  :: [ChartData] } deriving Show
 
-type ChartM a = State Chart a
+newtype ChartM a = ChartM {unChartM :: Chart -> a }
 
+instance Monad ChartM where
+  return x = ChartM $ \ _   ->  x
+  
+  m >>= k  = ChartM $ \chart -> let v = unChartM m chart
+                               in unChartM (k v) chart
 
 class ChartItem c where
-  set :: c -> ChartM ()
+  set :: c -> ChartM Chart
   encode :: c -> [(String,String)]
 
 
@@ -30,42 +39,61 @@ defaultChart = Chart { chartSize  = Size 320 200,
                        chartData  = [],
                        chartTitle = Nothing }
 
--- Setting Chart Data
+-- Setting/Encoding Chart Data
 
-updateChart u = do cd <- get
-                   put $ u cd
-                   return ()
+getChart = ChartM $ \chart -> chart
 
-asList a = [a]
+updateChart u = do chart <- getChart
+                   return $ u chart
 
+asList a = [a]                      
+
+
+-- size
 instance ChartItem ChartSize where
-    set size = updateChart $ \cd -> cd { chartSize = size }
+    set size = updateChart $ \chart -> chart { chartSize = size }
 
     encode size =  asList ("chs", show width ++ "x" ++ show height) where
                    Size width height = size
 
 
-setChartSize w h = updateChart $ \cd -> cd {  chartSize = Size w h }
+-- type
+instance ChartItem ChartType where
+    set cType = updateChart $ \chart -> chart { chartType = cType }
 
-setChartType t = updateChart $ \cd -> cd { chartType = t }
+    encode cType =  asList ("cht",t)
+                    where t = case cType of
+                                    Line       -> "lc"
+                                    LineXY     -> "lxy"
+                                    Sparklines -> "ls"
 
-setChartTitle t = updateChart $ \cd -> cd { chartTitle = Just t } 
+-- title
+instance ChartItem ChartTitle where
+    set title = updateChart $ \chart -> chart { chartTitle = title }
+
+    encode title = case title of
+                     Just t -> asList ("chl", t)
+                     _      -> []
+
+-- helper functions for syntactic sugar
+
+setChartSize :: Int -> Int -> ChartM Chart
+setChartSize w h = set (Size w h)
+
+setChartType :: ChartType -> ChartM Chart
+setChartType = set
+
+setChartTitle :: String -> ChartM Chart
+setChartTitle title = set (Just title)
+
+{-
+
 
 addChartData = updateChart . flip addDataToChart
 
 addDataToChart cd d = cd { chartData = old ++ [d] }
                       where old = chartData cd
 
--- Encoding Chart Data
-
-
-
--- type
-encodeChartType t = asList ("cht",cType)
-                    where cType = case t of
-                                    Line       -> "lc"
-                                    LineXY     -> "lxy"
-                                    Sparklines -> "ls"
 
 -- data
 encodeChartData datas = asList ("chd", encodeSimple $ normalise datas)
@@ -74,26 +102,16 @@ normalise datas = concatMap n datas
                   where n (D d) = [d]
                         n (XY d) = map fst d :  [map snd d]
 
--- size
-encodeChartSize size = asList ("chs", show width ++ "x" ++ show height) where
-                       Size width height = size
-
-
--- title
-encodeChartTitle (Just title) = asList ("chl",title)
-encodeChartTitle _ = []
-
+-}
 
 -- URL Conversion
-getParams chart = encodeChartType  (chartType  chart) ++ 
-                  encodeChartData  (chartData  chart) ++
-                  encodeChartTitle (chartTitle chart) ++
-                  encodeChartSize  (chartSize  chart)
+getParams chart =  concat $ [encode $ chartType chart,
+                             encode $ chartTitle chart,
+                             encode $ chartSize chart]
 
 convertToUrl chart = baseURL ++ intercalate "&" urlparams where
     baseURL = "http://chart.apis.google.com/chart?"
     urlparams = [urlEnc a ++ "=" ++ urlEnc b | (a,b) <- getParams chart]
-
 
 
 -- Data Encodings
@@ -122,11 +140,12 @@ urlEnc str = concatMap enc str where
 
 -- API Functions
 
-getChartData m = execState m defaultChart
+getChartData m = unChartM m $ defaultChart
 
 getUrl =  convertToUrl . getChartData
 
 debugChart = getUrl $ do setChartSize 320 200
                          setChartType Line
-                         addChartData $ D [1,2,3,4,5]
-                         addChartData $ D [3,4,5,6,7]
+                         setChartTitle "Test"
+                         -- addChartData $ D [1,2,3,4,5]
+                         -- addChartData $ D [3,4,5,6,7]
